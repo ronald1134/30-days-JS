e07871ca9701a6cbb9142fe392a2bd560092376f
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-04-09 22:43:37 -0300

    criando dois exercícios

---
+++ 1_dia/array_comida.js
@@ -0,0 +1,2 @@
+let comida = ['Bife à milanesa', 'Burgen bacon', 'carne moída']
+console.log(`A minha primeira comida favorita é "${comida[0]}🥩" e a segunda é "${comida[1]}🍔"`)
---
+++ 1_dia/desconto_valor.js
@@ -0,0 +1,11 @@
+let precoRtx = 1800
+let descontoPorcentagem = 10
+let valorDesconto = (precoRtx * descontoPorcentagem) / 100
+let valorFinal = precoRtx - valorDesconto
+
+console.log(`O preço da RTX está por R$${precoRtx} com o desconto de R$${descontoPorcentagem} e com isso ficará saindo por R$${valorFinal} a RTX`)
+
+/*let precoRtx = 1500;
+let desconto = 50;
+let precoFinal = precoRtx - desconto;
+console.log(`O preço da RTX está por ${precoRtx} com o desconto de ${desconto}, com isso o preço final ficará por ${precoFinal}`)*/
