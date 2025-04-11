8d6a2e967458f41e4ee03bd0025554d9c9dbe9b4
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-04-10 22:36:05 -0300

    feito o dia dois
    colocando em pratica com if, else if e else.
    e o outro utilizando switch

---
+++ 2_dia_condicionais/condição_dia.js
@@ -0,0 +1,13 @@
+/*
+PARA PEGAR O HORÁRIO ATUAL
+const horaAtual = new Date()
+let Hora = horaAtual.getHours
+*/
+const hora = 19  //para definir o horário fixo
+if (hora < 12){
+    console.log('Bom diaaa!☀️')
+}else if(hora < 19){
+    console.log('Boa tardeee!🌇')
+}else{
+    console.log('Boa noiteee!🌆')
+}
---
+++ 2_dia_condicionais/switch_meses.js
@@ -0,0 +1,42 @@
+//um sistema usando a condição de switch para puxar meses dependendo do valor da variável
+let mes = 0
+switch (mes) {
+    case 1:
+        console.log('Estamos no mês de Janeiro (mês 1)')
+        break;
+    case 2:
+        console.log('estamos no mês de Fevereiro (mês 2)')
+        break;
+    case 3:
+        console.log('Estamos no mês de Março (mês 3)')
+        break;
+    case 4:
+        console.log('Estamos no mês de Abril (mês 4)')
+        break;
+    case 5:
+        console.log('Estamos no mês de Maio (mês 5)')
+        break;
+    case 6:
+        console.log('Estamos no mês de Junho (mês 6)')
+        break;
+    case 7:
+        console.log('Estamos no mês de Julho (mês 7)')
+        break;
+    case 8:
+        console.log('Estamos no mês de Agosto (mês 8)')
+        break;
+    case 9:
+        console.log('Estamos no mês de Setembro (mês 9)')
+        break;
+    case 10:
+        console.log('Estamos no mês de Outubro (mês 10)')
+        break;
+    case 11:
+        console.log('Estamos no mês de Novembro (mês 11)')
+        break;
+    case 12:
+        console.log('Estamos no mês de Dezembro (mês 12)')
+        break;
+    default: console.log("Mês inválido! Digite um valor entre 1 e 12! 📝")
+}
+//depois irei tentar utilizar array para deixar mais simplificado!
