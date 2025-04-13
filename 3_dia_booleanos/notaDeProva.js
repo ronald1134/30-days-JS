b1e9716ce623cfaa6d0d5a855646f5cac3c28032
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-04-13 01:19:00 -0300

    atualizando

--- 3_dia_booleanos/notaDeProva.js
+++ 3_dia_booleanos/notaDeProva.js
@@ -1,9 +1,9 @@
-let Notas = 60
+let Notas = 70
 console.log(`Na prova que você realizou, você tirou ${Notas} `)
 if( Notas >= 90){
-    console.log('Você foi aprovado com a categoria A!! Parabéns ✅')
+    console.log('Você foi aprovado com a categoria "A" Parabéns ✅')
 }else if (Notas >= 70 && Notas < 90){
-    console.log('Você foi aprovado com a categoria "B"! Parabéns ✅')
+    console.log('Você foi aprovado com a categoria "B" Parabéns ✅')
 }else{
     console.log('Infelizmente você reprovou, tente novamente nas proxima liberações do ano que vem..❌')
 }
